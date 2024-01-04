d0ab0e145fca225a8f313c9334059b6ddbb433cb
fer0n <rnichi@me.com>
2024-01-04 14:13:01 +0100

    Added TabBar
    - works with sheet
    - correct color

---
+++ Unwatched/View/QueueView.swift
@@ -0,0 +1,24 @@
+//
+//  QueueView.swift
+//  Unwatched
+//
+
+import SwiftUI
+
+struct QueueView: View {
+    var onVideoTap: (_ video: Video) -> Void;
+    @Environment(VideoManager.self) var videoManager
+    
+    var body: some View {
+        List(videoManager.videos) { video in
+            VideoListItem(video: video)
+             .onTapGesture {
+                 onVideoTap(video);
+             }
+        }
+    }
+}
+
+//#Preview {
+//    QueueView()
+//}
