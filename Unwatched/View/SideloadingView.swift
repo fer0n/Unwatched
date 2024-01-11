d3bb5ce7c5a5c24bf1afaf0d99a147e3c423f742
fer0n <rnichi@me.com>
2024-01-11 13:20:32 +0100

    swipeActions refactor

--- Unwatched/View/SideloadingView.swift
+++ Unwatched/View/SideloadingView.swift
@@ -2,15 +2,7 @@ import SwiftUI
 import SwiftData
 
 struct SideloadingView: View {
-    @Environment(\.modelContext) var modelContext
     @Query(filter: #Predicate<Video> { $0.subscription == nil }) var sideloadedVideos: [Video]
-    @Query var queue: [QueueEntry]
-
-    func addVideoToQueue(_ video: Video) {
-        VideoService.insertQueueEntries(at: 0,
-                                        videos: [video],
-                                        modelContext: modelContext)
-    }
 
     var body: some View {
         ZStack {
@@ -19,24 +11,7 @@ struct SideloadingView: View {
             } else {
                 List {
                     ForEach(sideloadedVideos) { video in
-                        VideoListItem(video: video, showVideoStatus: true)
-                            .swipeActions(edge: .leading, allowsFullSwipe: true) {
-                                Button {
-                                    addVideoToQueue(video)
-                                } label: {
-                                    Image(systemName: "text.badge.plus")
-                                }
-                                .tint(.teal)
-                            }
-                            .swipeActions(edge: .trailing) {
-                                Button {
-                                    VideoService.clearFromEverywhere(video,
-                                                                     modelContext: modelContext)
-                                } label: {
-                                    Image(systemName: "xmark.circle.fill")
-                                }
-                                .tint(.orange)
-                            }
+                        VideoListItem(video: video, showVideoStatus: true, videoSwipeActions: [.queue, .clear])
                     }
                 }
                 .listStyle(.plain)
--- Unwatched/View/SubscriptionDetailView.swift
+++ Unwatched/View/SubscriptionDetailView.swift
@@ -11,13 +11,6 @@ struct SubscriptionDetailView: View {
     @Environment(\.modelContext) var modelContext
     @AppStorage("defaultVideoPlacement") var defaultVideoPlacement: VideoPlacement = .inbox
 
-    func addVideoToQueue(_ video: Video) {
-        VideoService.insertQueueEntries(
-            videos: [video],
-            modelContext: modelContext)
-        // TODO: potentially delete inbox entry here?
-    }
-
     var body: some View {
         VStack {
             List {
@@ -34,24 +27,7 @@ struct SubscriptionDetailView: View {
                     ForEach(subscription.videos.sorted(by: { ($0.publishedDate ?? Date.distantPast)
                                                         > ($1.publishedDate ?? Date.distantPast)})
                     ) { video in
-                        VideoListItem(video: video, showVideoStatus: true)
-                            .swipeActions(edge: .leading, allowsFullSwipe: true) {
-                                Button {
-                                    addVideoToQueue(video)
-                                } label: {
-                                    Image(systemName: "text.badge.plus")
-                                }
-                                .tint(.teal)
-                            }
-                            .swipeActions(edge: .trailing) {
-                                Button {
-                                    VideoService.clearFromEverywhere(video,
-                                                                     modelContext: modelContext)
-                                } label: {
-                                    Image(systemName: "xmark.circle.fill")
-                                }
-                                .tint(.orange)
-                            }
+                        VideoListItem(video: video, showVideoStatus: true, videoSwipeActions: [.queue, .clear])
                     }
                 }
             }
