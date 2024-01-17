fb72d1b73969ec4050dcda2dc47775dead31e753
fer0n <rnichi@me.com>
2024-01-17 08:02:21 +0100

    Load subscriptions in parallel

--- Unwatched/Data/SubscriptionService.swift
+++ Unwatched/Data/SubscriptionService.swift
@@ -3,7 +3,6 @@ import SwiftData
 
 class SubscriptionService {
     static func addSubscriptions(from urls: [URL], modelContainer: ModelContainer) async throws -> [SubscriptionState] {
-        print("addSubscriptionsInBg")
         // TODO: should this run in the Bg? Maybe return a task with the subsciptionStates
         // maybe even an array of tasks that automatically updates the UI?
         let repo = SubscriptionActor(modelContainer: modelContainer)
