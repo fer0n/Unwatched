e7688dc3a59f84c8b205b75cf4d0a1bfbfa304f5
fer0n <rnichi@me.com>
2024-01-26 13:51:19 +0100

    improved sheet swipe hight detection
    No longer relies on timer waiting on sheet to appear

--- Unwatched/Helper/SheetPositionReader.swift
+++ Unwatched/Helper/SheetPositionReader.swift
@@ -9,29 +9,19 @@ import Foundation
     // Sheet animation and height detection
     var swipedBelow: Bool = true
     @ObservationIgnored var sheetHeight: CGFloat = .zero
-    @ObservationIgnored var normalSheetHeight: CGFloat = .zero
-    @ObservationIgnored var sheetSwipeHeight: CGFloat = .zero
+    @ObservationIgnored private var sheetDistanceToTop: CGFloat = .zero
 
     var maxSheetHeight: CGFloat {
         sheetHeight - Const.playerAboveSheetHeight
     }
 
-    // will be called onAppear to find out the normal global position of the sheet
-    func setNormalSheetHeightDelayed() {
-        Task {
-            // Wait for the sheet to be fully open
-            await Task.sleep(s: 0.5)
-            await MainActor.run {
-                normalSheetHeight = sheetSwipeHeight
-                handleSheetMinYUpdate(sheetSwipeHeight) // manually trigger update
-            }
-        }
+    func setTopSafeArea(_ topSafeArea: CGFloat) {
+        sheetDistanceToTop = topSafeArea + Const.playerAboveSheetHeight
     }
 
     // global position changes
     func handleSheetMinYUpdate(_ minY: CGFloat) {
-        sheetSwipeHeight = minY
-        let value = minY - normalSheetHeight
+        let value = minY - sheetDistanceToTop
         let newBelow = value > 50 || minY == 0 // after dismissing the sheet minY becomes 0
         if newBelow != swipedBelow {
             swipedBelow = newBelow
