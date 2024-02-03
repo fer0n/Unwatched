1780700716a0afb8c2254c29787c2e58feb86167
fer0n <rnichi@me.com>
2024-02-03 22:37:30 +0100

    tiny refresh animation improvement

--- Unwatched/RefreshToolbarButton.swift
+++ Unwatched/RefreshToolbarButton.swift
@@ -9,6 +9,7 @@ import SwiftData
 struct RefreshToolbarButton: ToolbarContent {
     @Environment(RefreshManager.self) var refresher
     var refreshOnlySubscription: PersistentIdentifier?
+    @State var isLoading = false
 
     var body: some ToolbarContent {
         ToolbarItem(placement: .topBarTrailing) {
@@ -21,15 +22,32 @@ struct RefreshToolbarButton: ToolbarContent {
             } label: {
                 Image(systemName: Const.refreshSF)
                     .font(.system(size: 13))
-                    .rotationEffect(refresher.isLoading ? .degrees(360) : .degrees(0))
+                    .rotationEffect(isLoading ? .degrees(180) : .degrees(0))
                     .animation(
-                        refresher.isLoading ? Animation.linear(duration: 2).repeatForever(autoreverses: false) :
-                            .default, value: refresher.isLoading)
+                        isLoading ? Animation.linear(duration: 1).repeatForever(autoreverses: false) :
+                            .default, value: isLoading)
+            }
+            .onAppear {
+                guard isLoading != refresher.isLoading else {
+                    return
+                }
+                withAnimation {
+                    isLoading = refresher.isLoading
+                }
+            }
+            .onChange(of: refresher.isLoading) {
+                withAnimation {
+                    isLoading = refresher.isLoading
+                }
             }
         }
     }
 }
 
 // #Preview {
+//     Image(systemName: Const.refreshSF)
+//         .font(.system(size: 13))
+//         .symbolEffect(.variableColor.iterative, options: .repeating, value: true)
 //    RefreshToolbarButton()
+//         .environment(RefreshManager())
 // }
