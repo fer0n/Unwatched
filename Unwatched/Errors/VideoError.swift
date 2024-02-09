99961981dfbcdc51170fdbdfa94c2d8cc5a9eb9c
fer0n <rnichi@me.com>
2024-02-09 16:03:12 +0100

    fix: getting video id from youtu.be urls

--- Unwatched/Errors/VideoError.swift
+++ Unwatched/Errors/VideoError.swift
@@ -9,6 +9,7 @@ enum VideoError: LocalizedError {
     case noVideoFound
     case noYoutubeId
     case faultyYoutubeVideoId(_ youtubeId: String)
+    case emptyYoutubeId
 
     var errorDescription: String? {
         switch self {
@@ -18,6 +19,8 @@ enum VideoError: LocalizedError {
             return NSLocalizedString("noYoutubeIdFound", comment: "")
         case .faultyYoutubeVideoId(let youtubeId):
             return NSLocalizedString("potentiallyFaultyYoutubeId", comment: "\(youtubeId)")
+        case .emptyYoutubeId:
+            return NSLocalizedString("emptyYoutubeId", comment: "")
         }
     }
 }
