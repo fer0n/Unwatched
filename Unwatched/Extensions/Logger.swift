383c7609479f1c463a776b52d8c7c6741f5a09b0
fer0n <rnichi@me.com>
2024-03-06 16:56:07 +0100

    fix logger error

--- Unwatched/Extensions/Logger.swift
+++ Unwatched/Extensions/Logger.swift
@@ -7,13 +7,8 @@ import Foundation
 import OSLog
 
 extension Logger: @unchecked Sendable {
-    /// Using your bundle identifier is a great way to ensure a unique identifier.
-    private static var subsystem: String {
-        return Bundle.main.bundleIdentifier ?? "default"
-    }
-
     /// Logs the view cycles like a view that appeared.
-    static let log = Logger(subsystem: subsystem, category: "viewcycle")
+    static let log = Logger(subsystem: "com.pentlandFirth.Unwatched", category: "viewcycle")
 }
 
 #if swift(>=6.0)
