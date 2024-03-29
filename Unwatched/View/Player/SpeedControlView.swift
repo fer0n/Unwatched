12b9d4e70bb6ae43653d31599201a783b689eaa8
fer0n <rnichi@me.com>
2024-03-29 23:08:03 +0100

    fix: SpeedControl crash .infinity/.NaN; Refactor & unit test

--- Unwatched/View/Player/SpeedControlView.swift
+++ Unwatched/View/Player/SpeedControlView.swift
@@ -8,23 +8,19 @@ import SwiftUI
 struct SpeedControlView: View {
     @Environment(NavigationManager.self) private var navManager
 
+    @State var viewModel = SpeedControlViewModel()
+    @State var hapticToggle = false
+    @State var controlMinX: CGFloat?
+    @State private var dragState: CGFloat?
+
     @Binding var selectedSpeed: Double
 
-    static let speeds: [Double] = [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2]
     let highlighted: [Double] = [1, 1.5, 2]
-
     static let padding: CGFloat = 0
     static let maxHeight: CGFloat = 40
     static let midY: CGFloat = SpeedControlView.maxHeight / 2
-
     let frameHeight: CGFloat = 30
     let coordinateSpace: NamedCoordinateSpace = .named("speed")
-    @State var hapticToggle = false
-    @State var width: CGFloat = 0
-    @State var itemWidth: CGFloat = 0
-
-    @State var controlMinX: CGFloat?
-    @State private var dragState: CGFloat?
 
     var body: some View {
         ZStack {
@@ -34,7 +30,7 @@ struct SpeedControlView: View {
                 .frame(maxWidth: .infinity)
 
             HStack(spacing: 0) {
-                ForEach(SpeedControlView.speeds, id: \.self) { speed in
+                ForEach(SpeedControlViewModel.speeds, id: \.self) { speed in
                     let isHightlighted = highlighted.contains(speed)
                     let frameSize: CGFloat = 5
                     let foregroundColor: Color = .foregroundGray
@@ -50,13 +46,13 @@ struct SpeedControlView: View {
                             .onTapGesture {
                                 withAnimation {
                                     selectedSpeed = speed
-                                    controlMinX = getXPos(width, selectedSpeed)
+                                    controlMinX = viewModel.getXPos(viewModel.width, selectedSpeed)
                                     hapticToggle.toggle()
                                     dragState = nil
                                 }
                             }
                         if isHightlighted {
-                            Text(SpeedControlView.formatSpeed(speed))
+                            Text(SpeedControlViewModel.formatSpeed(speed))
                                 .fontWeight(.bold)
                                 .font(.system(size: 12))
                                 .foregroundStyle(foregroundColor)
@@ -70,9 +66,9 @@ struct SpeedControlView: View {
                 }
             }
             .onPreferenceChange(SpeedPreferenceKey.self) { minY in
-                self.width = minY.width
-                self.itemWidth = width / CGFloat(SpeedControlView.speeds.count)
-                controlMinX = getXPos(width, selectedSpeed)
+                viewModel.width = minY.width
+                viewModel.itemWidth = viewModel.width / CGFloat(SpeedControlViewModel.speeds.count)
+                controlMinX = viewModel.getXPos(viewModel.width, selectedSpeed)
             }
             .padding(.horizontal, SpeedControlView.padding)
 
@@ -93,7 +89,7 @@ struct SpeedControlView: View {
             }
         }
         .onChange(of: selectedSpeed) {
-            controlMinX = getXPos(width, selectedSpeed)
+            controlMinX = viewModel.getXPos(viewModel.width, selectedSpeed)
         }
         .onChange(of: navManager.showMenu) {
             if dragState != nil {
@@ -106,18 +102,18 @@ struct SpeedControlView: View {
             DragGesture(minimumDistance: 2, coordinateSpace: coordinateSpace)
                 .onChanged { gesture in
                     let dragPosition = (controlMinX ?? 0) + gesture.translation.width
-                    let cappedMax = max(dragPosition, SpeedControlView.padding + (itemWidth / 2))
-                    dragState = min(cappedMax, width - (SpeedControlView.padding + (itemWidth / 2)))
+                    let cappedMax = max(dragPosition, SpeedControlView.padding + (viewModel.itemWidth / 2))
+                    dragState = min(cappedMax, viewModel.width - (SpeedControlView.padding + (viewModel.itemWidth / 2)))
                 }
                 .onEnded { state in
                     let value = state.translation.width
                     let currentPos = (controlMinX ?? 0) + value
-                    let selected = getSpeedFromPos(currentPos)
+                    let selected = viewModel.getSpeedFromPos(currentPos)
                     controlMinX = currentPos
                     selectedSpeed = selected
 
                     withAnimation {
-                        controlMinX = getXPos(width, selected)
+                        controlMinX = viewModel.getXPos(viewModel.width, selected)
                         hapticToggle.toggle()
                         dragState = nil
                     }
@@ -130,36 +126,15 @@ struct SpeedControlView: View {
 
     func getFloatingText() -> String {
         if dragState == nil {
-            return SpeedControlView.formatSpeed(selectedSpeed) + "×"
+            return SpeedControlViewModel.formatSpeed(selectedSpeed) + "×"
         }
-        let speed = getSpeedFromPos(dragState ?? 0)
-        return SpeedControlView.formatSpeed(speed) + "×"
-    }
-
-    func getSpeedFromPos(_ pos: CGFloat) -> Double {
-        let itemWidth = width / CGFloat(SpeedControlView.speeds.count)
-        let calculatedIndex = Int(round((pos / itemWidth) - 0.5) )
-        let index = max(0, min(calculatedIndex, SpeedControlView.speeds.count - 1))
-        let speed = SpeedControlView.speeds[index]
-        return speed
-    }
-
-    func getXPos(_ fullWidth: CGFloat, _ speed: CGFloat) -> CGFloat {
-        let selectedSpeedIndex = SpeedControlView.speeds.firstIndex(of: speed) ?? 0
-        return SpeedControlView.padding + (CGFloat(selectedSpeedIndex) * itemWidth) + (itemWidth / 2)
-    }
-
-    static func formatSpeed(_ speed: Double) -> String {
-        if floor(speed) == speed {
-            return String(format: "%.0f", speed)
-        } else {
-            return String(format: "%.1f", speed)
-        }
+        let speed = viewModel.getSpeedFromPos(dragState ?? 0)
+        return SpeedControlViewModel.formatSpeed(speed) + "×"
     }
 }
 
 struct SpeedPreferenceKey: PreferenceKey {
-    static var defaultValue: CGRect = .zero
+    static var defaultValue: CGRect { .zero }
     static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
         value = nextValue()
     }
