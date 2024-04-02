6d921cd410dc023c4088dadfaff40f2102b4a643
fer0n <rnichi@me.com>
2024-04-02 08:29:10 +0200

    Added sentry

--- Unwatched/UnwatchedApp.swift
+++ Unwatched/UnwatchedApp.swift
@@ -4,11 +4,24 @@
 //
 
 import SwiftUI
+import Sentry
+
 import SwiftData
 import TipKit
 
 @main
 struct UnwatchedApp: App {
+    init() {
+        SentrySDK.start { options in
+            options.dsn = "https://11c24101cf9f18267a19723b114d20e0@o4507015359430656.ingest.us.sentry.io/4507015361986560"
+            options.debug = true // Enabled debug when first installing is always helpful
+            options.enableTracing = true
+
+            options.attachScreenshot = true // This adds a screenshot to the error events
+            options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+            options.swiftAsyncStacktraces = true
+        }
+    }
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var sharedModelContainer: ModelContainer = {
