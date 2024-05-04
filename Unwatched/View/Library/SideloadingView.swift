bc3b6f0e1fec22414367929c5390ae659be283f2
fer0n <rnichi@me.com>
2024-05-04 15:22:06 +0200

    Sideloading view shows video list

--- Unwatched/View/Library/SideloadingView.swift
+++ Unwatched/View/Library/SideloadingView.swift
@@ -9,19 +9,30 @@ struct SideloadingView: View {
 
     var body: some View {
         let subs = sidedloadedSubscriptions.filter({ $0.videos?.isEmpty == false })
+
         ZStack {
             if subs.isEmpty {
                 ContentUnavailableView("noSideloadedSubscriptions",
                                        systemImage: "arrow.right.circle",
                                        description: Text("noSideloadedSubscriptionsDetail"))
             } else {
+                let videos = subs.flatMap { $0.videos ?? [] }
+                let past = Date.distantPast
+                let sortedVideos = videos.sorted(by: { $0.publishedDate ?? past > $1.publishedDate ?? past })
                 List {
-                    SubscriptionListView(
-                        sort: .title,
-                        filter: #Predicate<Subscription> { $0.isArchived == true },
-                        manualFilter: { $0.videos?.isEmpty == false }
-                    )
+                    ForEach(sortedVideos) { video in
+                        VideoListItem(
+                            video,
+                            config: VideoListItemConfig(
+                                showVideoStatus: true,
+                                hasInboxEntry: video.inboxEntry != nil,
+                                hasQueueEntry: video.queueEntry != nil,
+                                watched: video.watched
+                            )
+                        )
+                    }
                 }
+                .listStyle(.plain)
                 .navigationTitle("sideloads")
                 .navigationBarTitleDisplayMode(.inline)
             }
