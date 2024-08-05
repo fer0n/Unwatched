e9454c81c9df4df15c86356dfcc8491f26e7f0c1
fer0n <rnichi@me.com>
2024-08-05 07:17:40 +0200

    Added data fetch performance test

---
+++ UnwatchedUITests/DataSpeedTests.swift
@@ -0,0 +1,51 @@
+//
+//  DataSpeedTests.swift
+//  Unwatched
+//
+
+import XCTest
+import SwiftData
+
+// swiftlint:disable all
+final class DataSpeedTests: XCTestCase {
+
+    func testFilterShorts() async {
+        let modelContext = await getModelContext()
+        let sort = SortDescriptor<Video>(\.clearedInboxDate, order: .reverse)
+        let filter = await VideoListView.getVideoFilter(nil, .moderate)
+        let fetch = FetchDescriptor<Video>(predicate: filter, sortBy: [sort])
+
+        measure {
+            guard let videos = try? modelContext.fetch(fetch) else {
+                XCTFail("Failed to fetch videos")
+                return
+            }
+
+            print("Videos: \(videos.count)")
+        }
+    }
+    
+    // MARK: Helpers
+    
+    func getModelContext() async -> ModelContext {
+        let container = await DataController.previewContainer
+        let data = (try? loadFileData(for: "backup-file.json"))!
+        UserDataService.importBackup(data, container: container)
+        return ModelContext(container)
+    }
+
+    func loadFileData(for fixture: String) throws -> Data {
+        let url = testDataDirectory().appendingPathComponent(fixture)
+        return try Data(contentsOf: url)
+    }
+
+    func testDataDirectory(path: String = #file) -> URL {
+        let url = URL(fileURLWithPath: path)
+        let testsDir = url.deletingLastPathComponent()
+        let res = testsDir.appendingPathComponent("TestData")
+        return res
+    }
+
+}
+
+// swiftlint:enable all
