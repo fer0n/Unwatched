72f567be215b9227dd99d8e37bfc680b111b31fa
fer0n <rnichi@me.com>
2024-08-24 10:22:31 +0200

    list item ui adjustments

--- Unwatched/View/VideoListItem/VideoListItem.swift
+++ Unwatched/View/VideoListItem/VideoListItem.swift
@@ -32,10 +32,16 @@ struct VideoListItem: View {
                     Color.backgroundColor
                         .frame(width: 168, height: 94.5)
                 }
+                .overlay {
+                    VideoListItemThumbnailOverlay(
+                        video: video,
+                        videoDuration: config.videoDuration
+                    )
+                }
                 .clipShape(.rect(cornerRadius: 15.0))
                 .padding([.vertical, .leading], config.showVideoStatus ? 5 : 0)
 
-                VideoListItemDetails(video: video, videoDuration: config.videoDuration)
+                VideoListItemDetails(video: video)
             }
             VideoListItemStatus(
                 video: video,
@@ -51,6 +57,74 @@ struct VideoListItem: View {
         .modifier(VideoListItemSwipeActionsModifier(
                     video: video,
                     config: config))
+    }
+}
+
+struct VideoListItemThumbnailOverlay: View {
+    @AppStorage(Const.themeColor) var theme: ThemeColor = Color.defaultTheme
+
+    let video: Video
+    let videoDuration: Double?
+    // workaround: doesn't update instantly otherwise
+
+    var body: some View {
+        let elapsed = video.elapsedSeconds
+        let total = videoDuration ?? video.duration
+
+        ZStack {
+            if let elapsed = elapsed, let total = total {
+                let progress = elapsed / total
+                GeometryReader { geo in
+                    let progressWidth = geo.size.width * progress
+
+                    VStack(spacing: 0) {
+                        Spacer()
+                        Color.black
+                            .frame(height: 2)
+                            .opacity(0.2)
+                            .mask(LinearGradient(gradient: Gradient(
+                                stops: [
+                                    .init(color: .black, location: 0),
+                                    .init(color: .clear, location: 1)
+                                ]
+                            ), startPoint: .bottom, endPoint: .top))
+                        HStack(spacing: 0) {
+                            theme.color
+                                .frame(width: progressWidth)
+                            Color.clear.background(.thinMaterial)
+                        }
+                        .frame(height: 3)
+                    }
+                }
+            }
+
+            if total != nil || video.isYtShort {
+                ZStack {
+                    if video.isYtShort {
+                        Text(verbatim: "#s")
+                    } else if let time = total?.formattedSecondsColon {
+                        Text(time)
+                    }
+                }
+                .font(.system(size: 14))
+                .foregroundStyle(.secondary)
+                .padding(.horizontal, 3)
+                .background(.thinMaterial)
+                .clipShape(
+                    RoundedRectangle(
+                        cornerRadius: 3,
+                        style: .continuous
+                    )
+                )
+                .padding(.bottom, (total == nil || elapsed == nil) ? 4 : 8)
+                .padding(.trailing, 4)
+                .frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity,
+                    alignment: .bottomTrailing
+                )
+            }
+        }
     }
 }
 
@@ -84,6 +158,9 @@ struct VideoListItemConfig {
     guard let video = videos?.first else {
         return Text("noVideoFound")
     }
+    video.duration = 130
+    video.elapsedSeconds = 20
+    // video.isYtShort = true
 
     return List {
         VideoListItem(
--- Unwatched/View/VideoListItem/VideoListItemDetails.swift
+++ Unwatched/View/VideoListItem/VideoListItemDetails.swift
@@ -8,32 +8,18 @@ import SwiftUI
 struct VideoListItemDetails: View {
     @Environment(NavigationManager.self) private var navManager
     var video: Video
-    var videoDuration: Double?
-    // workaround: doesn't update instantly otherwise
 
     var body: some View {
         VStack(alignment: .leading, spacing: 3) {
             let videoTitle = !video.title.isEmpty ? video.title : video.youtubeId
             Text(videoTitle)
-                .frame(maxWidth: .infinity, alignment: .leading)
                 .font(.system(size: 15, weight: .semibold))
-                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .lineLimit(3)
                 .foregroundStyle(.primary)
-            if video.publishedDate != nil || video.isYtShort {
-                HStack {
-                    if let published = video.publishedDate {
-                        Text(published.formattedRelative)
-                            .font(.system(size: 14, weight: .light))
-                            .font(.body)
-                    }
-                    if video.isYtShort {
-                        Text(verbatim: "#s")
-                            .font(.system(size: 14, weight: .regular))
-                    }
-                }
-            }
-            if let title = video.subscription?.displayTitle {
-                Text(title)
+
+            if let subTitle = video.subscription?.displayTitle {
+                Text(subTitle)
                     .font(.system(size: 14, weight: .regular))
                     .lineLimit(1)
                     .textCase(.uppercase)
@@ -43,33 +29,13 @@ struct VideoListItemDetails: View {
                         }
                     }
             }
-            VideoListItemProgress(video: video, videoDuration: videoDuration)
-        }
-        .foregroundStyle(.secondary)
-    }
-}
 
-struct VideoListItemProgress: View {
-    let video: Video
-    let videoDuration: Double?
-
-    var body: some View {
-        ZStack {
-            if let duration = videoDuration ?? video.duration,
-               let remaining = video.remainingTime,
-               duration > 0 && remaining > 0 {
-                HStack(alignment: .center) {
-                    ProgressView(value: video.elapsedSeconds ?? 0, total: duration)
-                        .opacity(0.6)
-                        .padding(.top, 3)
-                        .padding(.trailing, 5)
-                    if video.hasFinished != true, let remaining = remaining.formattedSeconds {
-                        Text(verbatim: "-\(remaining)")
-                            .font(.system(size: 11, weight: .regular))
-                    }
-                }
+            if let published = video.publishedDate {
+                Text("\(published.formattedRelative) ago")
+                    .font(.system(size: 12, weight: .regular))
             }
         }
+        .foregroundStyle(.secondary)
     }
 }
 
