6b4689d263289c1dd0b9058721581a74223bb613
fer0n <rnichi@me.com>
2024-08-31 13:00:14 +0200

    sideloads → unsubscribed videos; all videos icon

--- Unwatched/View/Library/AllVideosView.swift
+++ Unwatched/View/Library/AllVideosView.swift
@@ -16,7 +16,7 @@ struct AllVideosView: View {
 
             SearchableVideos(text: $text)
             ContentUnavailableView("noVideosYet",
-                                   systemImage: "play.rectangle.on.rectangle",
+                                   systemImage: Const.allVideosViewSF,
                                    description: Text("noVideosYetDescription"))
                 .opacity(videos.isEmpty ? 1 : 0)
             List {
