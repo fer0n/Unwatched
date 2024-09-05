702f5f9697e32e99dce0f41124d57882b1b74473
fer0n <rnichi@me.com>
2024-09-05 05:56:09 +0200

    fix: added migration for cachedImage container

--- Unwatched/Model/VersionedSchema.swift
+++ Unwatched/Model/VersionedSchema.swift
@@ -42,3 +42,33 @@ enum UnwatchedMigrationPlan: SchemaMigrationPlan {
         [migrateV1toV1p1]
     }
 }
+
+// MARK: CachedImageSchema
+
+enum CachedImageSchemaV1p1: VersionedSchema {
+    static var versionIdentifier = Schema.Version(1, 1, 0)
+
+    static var models: [any PersistentModel.Type] {
+        [CachedImage.self]
+    }
+}
+
+enum CachedImageMigrationPlan: SchemaMigrationPlan {
+    static var schemas: [any VersionedSchema.Type] {
+        [CachedImageSchemaV1.self, CachedImageSchemaV1p1.self]
+    }
+
+    static let migrateCachedImageV1toV1p1 = MigrationStage.custom(
+        fromVersion: CachedImageSchemaV1.self,
+        toVersion: CachedImageSchemaV1p1.self,
+        willMigrate: { context in
+            // clear cache
+            try? context.delete(model: CachedImageSchemaV1.CachedImage.self)
+            try? context.save()
+        }, didMigrate: nil
+    )
+
+    static var stages: [MigrationStage] {
+        [migrateCachedImageV1toV1p1]
+    }
+}
