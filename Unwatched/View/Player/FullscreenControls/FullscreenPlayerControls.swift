d4a3a02d67a2c1403d8908db691545147457f25c
fer0n <rnichi@me.com>
2024-09-06 23:41:59 +0200

    moved fullscreen chapters into dedicated view

--- Unwatched/View/Player/FullscreenControls/FullscreenPlayerControls.swift
+++ Unwatched/View/Player/FullscreenControls/FullscreenPlayerControls.swift
@@ -7,7 +7,6 @@ import SwiftUI
 
 struct FullscreenPlayerControls: View {
     @Environment(PlayerManager.self) var player
-    @State var showChapters = false
     @Binding var menuOpen: Bool
 
     var markVideoWatched: (_ showMenu: Bool, _ source: VideoSource) -> Void
@@ -33,42 +32,7 @@ struct FullscreenPlayerControls: View {
 
             ZStack {
                 if hasChapters {
-                    Button {
-                        if !showChapters {
-                            showChapters = true
-                            menuOpen = true
-                        }
-                    } label: {
-                        Image(systemName: Const.chaptersSF)
-                            .modifier(PlayerControlButtonStyle())
-                    }
-                    .fontWeight(.bold)
-                    .accessibilityLabel("chapters")
-                }
-            }
-            .popover(isPresented: $showChapters) {
-                if let video = player.video {
-                    ZStack {
-                        Color.sheetBackground
-                            .scaleEffect(1.5)
-
-                        ScrollViewReader { proxy in
-                            ScrollView {
-                                ChapterList(video: video, isCompact: true)
-                                    .padding(6)
-                            }
-                            .onAppear {
-                                proxy.scrollTo(player.currentChapter?.persistentModelID, anchor: .center)
-                            }
-                            .scrollIndicators(.hidden)
-                        }
-                        .frame(minWidth: 200, maxWidth: 350)
-                    }
-                    .environment(\.colorScheme, .dark)
-                    .presentationCompactAdaptation(.popover)
-                    .onDisappear {
-                        menuOpen = false
-                    }
+                    FullscreenChaptersButton(menuOpen: $menuOpen)
                 }
             }
             .frame(maxHeight: .infinity)
