9e5d3acde148051ad406118ac4cbc5e547038054
fer0n <rnichi@me.com>
2024-09-08 09:34:39 +0200

    reduce @Query usage

--- Unwatched/View/Queue/QueueView.swift
+++ Unwatched/View/Queue/QueueView.swift
@@ -94,7 +94,7 @@ struct QueueView: View {
                 .foregroundStyle(theme.contrastColor)
                 .tint(theme.color)
 
-            DisappearingAddFeedsMenu()
+            AddFeedsMenu()
                 .bold()
                 .foregroundStyle(theme.contrastColor)
                 .tint(theme.color)
