ec2e4df62c98710949823ae75dd139ad4d07ea47
fer0n <rnichi@me.com>
2024-09-22 20:19:53 +0200

    flash next/prev icon as overlay

--- Unwatched/View/Player/FullscreenControls/OverlayFullscreenButton.swift
+++ Unwatched/View/Player/FullscreenControls/OverlayFullscreenButton.swift
@@ -5,10 +5,30 @@
 
 import SwiftUI
 
+@Observable class OverlayFullscreenVM {
+    @ObservationIgnored var hideControlsTask: (Task<(), Never>)?
+
+    var icon: OverlayIcon = .play
+    var show = false {
+        didSet {
+            Task {
+                if show {
+                    try? await Task.sleep(s: 0.2)
+                    show = false
+                }
+            }
+        }
+    }
+
+    func show(_ icon: OverlayIcon) {
+        self.icon = icon
+        show = true
+    }
+}
+
 struct OverlayFullscreenButton: View {
     @Environment(PlayerManager.self) var player
-    @State private var showPause = false
-    @State private var show = false
+    @Binding var overlayVM: OverlayFullscreenVM
 
     var enabled: Bool
     var landscapeFullscreen: Bool
@@ -21,28 +41,22 @@ struct OverlayFullscreenButton: View {
             .contentShape(Rectangle())
             .frame(width: touchSize, height: touchSize)
             .onTapGesture {
-                showPause = player.isPlaying
-                show = true
+                overlayVM.show(player.isPlaying ? .pause : .play)
                 player.handlePlayButton()
             }
             .opacity(enabled ? 1 : 0)
             .overlay {
-                Image(systemName: showPause ? "pause.circle.fill" : "play.circle.fill" )
+                Image(systemName: overlayVM.icon.systemName)
                     .resizable()
                     .frame(width: 90, height: 90)
-                    .animation(nil, value: show)
+                    .animation(nil, value: overlayVM.show)
                     .fontWeight(.black)
                     .symbolRenderingMode(.palette)
                     .foregroundStyle(.black, .white)
-                    .opacity(show && landscapeFullscreen ? 1 : 0)
-                    .task(id: show) {
-                        if show {
-                            try? await Task.sleep(s: 0.2)
-                            show = false
-                        }
-                    }
-                    .scaleEffect(show && landscapeFullscreen ? 1 : 0.7)
-                    .animation(.bouncy, value: show)
+                    .opacity(overlayVM.show && landscapeFullscreen ? 1 : 0)
+
+                    .scaleEffect(overlayVM.show && landscapeFullscreen ? 1 : 0.7)
+                    .animation(.bouncy, value: overlayVM.show)
                     .accessibilityLabel("playPause")
 
                 if player.videoEnded {
@@ -50,5 +64,21 @@ struct OverlayFullscreenButton: View {
                         .opacity(enabled && landscapeFullscreen ? 1 : 0)
                 }
             }
+    }
+}
+
+enum OverlayIcon {
+    case play
+    case pause
+    case next
+    case previous
+
+    var systemName: String {
+        switch self {
+        case .play: return "play.circle.fill"
+        case .pause: return "pause.circle.fill"
+        case .next: return "forward.end.circle.fill"
+        case .previous: return "backward.end.circle.fill"
+        }
     }
 }
