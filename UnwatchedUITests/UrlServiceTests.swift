5afaee0949ffc239ebda3893a7f3883ebd71e6d8
fer0n <rnichi@me.com>
2024-09-25 19:40:51 +0200

    fix: video position sometimes not updating correctly

---
+++ UnwatchedUITests/UrlServiceTests.swift
@@ -0,0 +1,37 @@
+//
+//  DataSpeedTests.swift
+//  Unwatched
+//
+
+import XCTest
+import SwiftData
+
+// swiftlint:disable all
+final class UrlServiceTests: XCTestCase {
+
+    func testExtractYoutubeId() async {
+
+        // https://youtu.be/dtp6b76pMak
+        // https://m.youtube.com/shorts/jH_QIBtX1gY
+        // https://www.youtube.com/watch?v=epBbbysk5cU
+        // https://piped.video/watch?v=VZIm_2MgdeA
+        // https://www.youtube.com/embed/Udl16tb2xv8?t=1414.0486603120037s&enablejsapi=1&color=white&controls=1&iv_load_policy=3
+        // youtube.com/watch?v=epBbbysk5cU
+
+        let testValues: [(String, String)] = [
+            ("https://youtu.be/dtp6b76pMak", "dtp6b76pMak"),
+            ("https://m.youtube.com/shorts/jH_QIBtX1gY", "jH_QIBtX1gY"),
+            ("https://www.youtube.com/watch?v=epBbbysk5cU", "epBbbysk5cU"),
+            ("https://piped.video/watch?v=VZIm_2MgdeA", "VZIm_2MgdeA"),
+            ("https://www.youtube.com/embed/Udl16tb2xv8?t=1414.0486603120037s&enablejsapi=1&color=white&controls=1&iv_load_policy=3", "Udl16tb2xv8"),
+            ("youtube.com/watch?v=epBbbysk5cU", "epBbbysk5cU")
+        ]
+
+        for (url, expected) in testValues {
+            let youtubeId = UrlService.getYoutubeIdFromUrl(url: URL(string: url)!)
+            XCTAssertEqual(youtubeId, expected)
+        }
+    }
+}
+
+// swiftlint:enable all
