e26ff0f7e7b32e61a6fe8014b17afcb24b198c64
fer0n <rnichi@me.com>
2024-10-14 14:40:21 +0200

    fix: video padding iPhone 16

--- Unwatched/Unwatched/Helper/fullscreenSafeArea.swift
+++ Unwatched/Unwatched/Helper/fullscreenSafeArea.swift
@@ -15,31 +15,30 @@ struct FullscreenSafeArea: ViewModifier {
     func body(content: Content) -> some View {
         content
             // scale down the video to avoid cutting into the notch/dynamic island
-            .padding([.top, .bottom], enable ? offset.edge : 0)
-            .offset(x: enable ? offset.x : 0)
+            .padding([.top, .bottom], enable ? offset : 0)
     }
 
-    var offset: (edge: CGFloat, x: CGFloat) {
+    var offset: CGFloat {
         switch device {
         case _ where device.contains("iPhone 16"):
-            return (7.3, 0)
+            return 7.8
         case _ where device.contains("iPhone 15"),
              _ where device.contains("iPhone 14 Pro"):
-            return (7.8, 0)
+            return 7.8
         case _ where device.contains("iPhone 14"),
              _ where device.contains("iPhone 13"),
              _ where device.contains("iPhone 12"),
              _ where device.contains("iPhone 11"),
              _ where device.contains("iPhone X"):
-            return (2, 0)
+            return 2
         default:
-            return (0, 0)
+            return 0
         }
     }
 }
 
 extension View {
-    func fullscreenSafeArea(enable: Bool, onlyOffsetRight: Bool = false) -> some View {
+    func fullscreenSafeArea(enable: Bool) -> some View {
         self.modifier(FullscreenSafeArea(enable: enable))
     }
 }
