5c85b07d52e962a4244b5dced75b918c3b02fcde
fer0n <rnichi@me.com>
2024-10-17 09:44:15 +0200

    fix: move to inbox animation

--- Unwatched/Unwatched/View/VideoListItem/VideoListItemSwipeActionsModifier.swift
+++ Unwatched/Unwatched/View/VideoListItem/VideoListItemSwipeActionsModifier.swift
@@ -98,8 +98,8 @@ struct VideoListItemSwipeActionsModifier: ViewModifier {
     }
 
     func moveToInbox() {
-        let task = VideoService.moveVideoToInbox(video, modelContext: modelContext)
-        handlePotentialQueueChange(after: task)
+        VideoService.moveVideoToInbox(video, modelContext: modelContext)
+        handlePotentialQueueChange()
         config.onChange?()
     }
 
