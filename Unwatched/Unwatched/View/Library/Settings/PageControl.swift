5284ca7ae88694690492b73fd64f37f4434b7304
fer0n <rnichi@me.com>
2024-10-26 20:26:08 +0200

    added tips in settings

---
+++ Unwatched/Unwatched/View/Library/Settings/PageControl.swift
@@ -0,0 +1,51 @@
+//
+//  PageControl.swift
+//  Unwatched
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    @Binding var currentPage: Int?
+    var numberOfPages: Int
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(currentPage: $currentPage)
+    }
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = 1
+        control.pageIndicatorTintColor = UIColor(.white)
+        control.currentPageIndicatorTintColor = UIColor(.white.opacity(0.5))
+        control.translatesAutoresizingMaskIntoConstraints = false
+        control.setContentHuggingPriority(.required, for: .horizontal)
+        control.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.pageControlDidFire(_:)),
+            for: .valueChanged)
+        return control
+    }
+
+    func updateUIView(_ control: UIPageControl, context: Context) {
+        context.coordinator.currentPage = $currentPage
+        control.numberOfPages = numberOfPages
+        if let currentPage {
+            control.currentPage = currentPage
+        }
+    }
+
+    class Coordinator {
+        var currentPage: Binding<Int?>
+
+        init(currentPage: Binding<Int?>) {
+            self.currentPage = currentPage
+        }
+
+        @objc
+        func pageControlDidFire(_ control: UIPageControl) {
+            currentPage.wrappedValue = control.currentPage
+        }
+    }
+}
