067358e1207f463bc824a90e204cc9849e276f40
fer0n <rnichi@me.com>
2024-11-01 17:33:13 +0100

    added shortcut to load info of currently selected video

--- Unwatched/Unwatched/AppShortcutsProvider.swift
+++ Unwatched/Unwatched/AppShortcutsProvider.swift
@@ -5,6 +5,7 @@ import UnwatchedShared
 
 struct AddYoutubeURL: AppIntent {
     static var title: LocalizedStringResource { "addYoutubeUrl" }
+    static let description = IntentDescription("addYoutubeUrlDescription")
 
     @Parameter(title: "youtubeUrl")
     var youtubeUrl: URL
@@ -21,7 +22,76 @@ struct AddYoutubeURL: AppIntent {
     }
 
     static var parameterSummary: some ParameterSummary {
-        Summary("Add video from \(\.$youtubeUrl)")
+        Summary("addYoutubeUrl \(\.$youtubeUrl)")
+    }
+}
+
+struct VideoEntityQuery: EntityQuery {
+    func entities(for identifiers: [VideoEntity.ID]) async throws -> [VideoEntity] {
+        []
+    }
+}
+
+struct VideoEntity: AppEntity {
+    let id: String
+
+    @Property(title: "videoTitle")
+    var title: String
+
+    @Property(title: "videoURL")
+    var url: URL?
+
+    @Property(title: "channelTitle")
+    var channelTitle: String?
+
+    var displayRepresentation: DisplayRepresentation {
+        DisplayRepresentation(stringLiteral: "\(title)")
+    }
+
+    static let typeDisplayRepresentation: TypeDisplayRepresentation = "Video"
+
+    static let defaultQuery = VideoEntityQuery()
+
+    init(
+        id: String,
+        title: String,
+        url: URL?,
+        channelTitle: String?
+    ) {
+        self.id = id
+        self.title = title
+        self.url = url
+        self.channelTitle = channelTitle
+    }
+}
+
+struct GetCurrentVideoInfo: AppIntent {
+    static var title: LocalizedStringResource { "getCurrentVideoInfo" }
+    static let description = IntentDescription("getCurrentVideoInfoDescription")
+
+    @MainActor
+    func perform() async throws -> some IntentResult & ReturnsValue<VideoEntity> {
+        let schema = Schema(DataController.dbEntries)
+        let container = try ModelContainer(for: schema, configurations: [DataController.modelConfig()])
+
+        let context = ModelContext(container)
+        let sort = SortDescriptor<QueueEntry>(\.order)
+        let fetch = FetchDescriptor<QueueEntry>(sortBy: [sort])
+        let entries = try context.fetch(fetch)
+        if let video = entries.first?.video {
+            let result = VideoEntity(
+                id: video.youtubeId,
+                title: video.title,
+                url: video.url,
+                channelTitle: video.subscription?.title
+            )
+            return .result(value: result)
+        }
+        throw VideoError.noVideoFound
+    }
+
+    static var parameterSummary: some ParameterSummary {
+        Summary("getCurrentVideoInfo")
     }
 }
 
@@ -32,6 +102,12 @@ struct UnwatchedAppShortcuts: AppShortcutsProvider {
             phrases: ["addYoutubeUrl"],
             shortTitle: "addYoutubeUrl",
             systemImageName: "play.rectangle.fill"
+        )
+        AppShortcut(
+            intent: GetCurrentVideoInfo(),
+            phrases: ["getCurrentVideoInfo"],
+            shortTitle: "getCurrentVideoInfo",
+            systemImageName: "info.circle.fill"
         )
     }
 }
