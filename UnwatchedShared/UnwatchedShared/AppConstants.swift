e22fbac4be741205fcda799b82229309f6b6fa73
fer0n <rnichi@me.com>
2025-03-31 06:54:57 +0200

    added option to automatically refresh despite active sync

--- UnwatchedShared/UnwatchedShared/AppConstants.swift
+++ UnwatchedShared/UnwatchedShared/AppConstants.swift
@@ -45,6 +45,7 @@ public struct Const {
     public static let aspectRatioTolerance: Double = 0.1
     public static let secondsConsideredCloseToEnd: CGFloat = 18
     public static let asyncListHistoryCheck = "asyncListHistoryCheck"
+    public static let autoRefreshIgnoresSync = "autoRefreshIgnoresSync"
     
     /// When seeking to the end, the video will seek to duraiton - thisBuffer
     public static let seekToEndBuffer: CGFloat = 0.5
