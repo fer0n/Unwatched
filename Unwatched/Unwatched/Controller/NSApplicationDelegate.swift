fba0105872bc72fd382ae265ee7650911272622e
fer0n <rnichi@me.com>
2025-04-06 18:13:17 +0200

    persist window frame position/size (macOS, #78)

--- Unwatched/Unwatched/Controller/NSApplicationDelegate.swift
+++ Unwatched/Unwatched/Controller/NSApplicationDelegate.swift
@@ -7,12 +7,47 @@
 import Foundation
 import AppKit
 import OSLog
+import UnwatchedShared
 
-class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
+class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject, NSWindowDelegate {
+    let mainWindow: NSWindow?
+
+    override init() {
+        mainWindow = nil
+        super.init()
+    }
+
     func applicationWillTerminate(_ notification: Notification) {
         Logger.log.info("applicationWillTerminate")
         SetupView.handleAppClosed()
+        persistWindowFrame()
         Logger.log.info("applicationWillTerminate done")
+    }
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Set window delegate to receive close notifications
+        NSApp.windows.first?.delegate = self
+
+        restoreWindowFrame()
+    }
+
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        persistWindowFrame()
+        return true
+    }
+
+    @MainActor
+    func restoreWindowFrame() {
+        if let frameDescription = UserDefaults.standard.string(forKey: Const.mainWindowFrame) {
+            mainWindow?.setFrame(from: frameDescription)
+        }
+    }
+
+    @MainActor
+    func persistWindowFrame() {
+        if let mainWindow = NSApp.windows.first {
+            UserDefaults.standard.set(mainWindow.frameDescriptor, forKey: Const.mainWindowFrame)
+        }
     }
 }
 #endif
