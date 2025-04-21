49bc6719dd3e74ae12480357a88859d50705a740
fer0n <rnichi@me.com>
2025-04-21 11:02:51 +0200

    new copy url options: timestamped, channel, playlist, rss feed

---
+++ Unwatched/Unwatched/View/Player/CopyUrlOptions.swift
@@ -0,0 +1,115 @@
+//
+//  CopyUrlMenu.swift
+//  Unwatched
+//
+
+import SwiftUI
+import UnwatchedShared
+
+struct CopyUrlOptions: View {
+    var asSection: Bool = false
+    var video: Video
+    var timestamp: Double?
+    var onSuccess: (() -> Void)?
+
+    var body: some View {
+        if asSection {
+            Section("copyUrl") {
+                options
+            }
+        } else {
+            Menu {
+                options
+            } label: {
+                Text("copyUrl")
+                Image(systemName: "document.on.document.fill")
+            }
+        }
+    }
+
+    @ViewBuilder
+    var options: some View {
+        copyUrlButton
+        copyUrlTimestampButton
+        copyPlaylistUrlButton
+        copyChannelUrlButton
+        copyRssFeedUrlButton
+    }
+
+    @ViewBuilder
+    var copyChannelUrlButton: some View {
+        if let channel = video.subscription,
+           let urlString = UrlService.getYoutubeUrl(
+            userName: channel.youtubeUserName,
+            channelId: channel.youtubeChannelId,
+            mobile: false,
+            videosSubPath: false
+           ) {
+            Button {
+                copyUrl(urlString)
+            } label: {
+                Text("channel")
+                Image(systemName: Const.channelSF)
+            }
+        }
+    }
+
+    @ViewBuilder
+    var copyPlaylistUrlButton: some View {
+        if let urlString = UrlService.getYoutubeUrl(
+            playlistId: video.subscription?.youtubePlaylistId,
+            mobile: false
+        ) {
+            Button {
+                copyUrl(urlString)
+            } label: {
+                Text("playlist")
+                Image(systemName: "list.bullet")
+            }
+        }
+    }
+
+    @ViewBuilder
+    var copyRssFeedUrlButton: some View {
+        if let urlString = video.subscription?.link?.absoluteString {
+            Button {
+                copyUrl(urlString)
+            } label: {
+                Text("rssFeed")
+                Image(systemName: "dot.radiowaves.up.forward")
+            }
+        }
+    }
+
+    @ViewBuilder
+    var copyUrlButton: some View {
+        Button {
+            let text = UrlService.getShortenedUrl(video.youtubeId)
+            copyUrl(text)
+        } label: {
+            Text("video")
+            Image(systemName: Const.videoSF)
+        }
+    }
+
+    @ViewBuilder
+    var copyUrlTimestampButton: some View {
+        if let timestamp = timestamp ?? video.elapsedSeconds {
+            Button {
+                let text = UrlService.getShortenedUrl(
+                    video.youtubeId,
+                    timestamp: timestamp
+                )
+                copyUrl(text)
+            } label: {
+                Text("videoAtTimestamp")
+                Image(systemName: "pin.fill")
+            }
+        }
+    }
+
+    func copyUrl(_ url: String) {
+        ClipboardService.set(url)
+        onSuccess?()
+    }
+}
--- UnwatchedShared/UnwatchedShared/AppConstants.swift
+++ UnwatchedShared/UnwatchedShared/AppConstants.swift
@@ -153,6 +153,9 @@ public struct Const {
     public static let disableFullscreenSF = "arrow.down.right.and.arrow.up.left"
 
     public static let reloadSF = "arrow.clockwise"
+    public static let shareSF = "square.and.arrow.up.fill"
+    public static let videoSF = "play.rectangle.fill"
+    public static let channelSF = "person.fill"
     
     // Settings
     public static let notificationsSettingsSF = "app.badge"
