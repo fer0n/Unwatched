06af36a2b9a9807938c34f356be5e7016ae1570c
fer0n <rnichi@me.com>
2025-06-14 00:35:33 +0200

    fix: macOS player size

--- Unwatched/Unwatched/View/SplitView.swift
+++ Unwatched/Unwatched/View/SplitView.swift
@@ -43,7 +43,7 @@ struct MacOSSplitView: View {
                 .environment(\.colorScheme, .dark)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            .edgesIgnoringSafeArea(.all)
+            .edgesIgnoringSafeArea(.vertical)
         }
     }
 
