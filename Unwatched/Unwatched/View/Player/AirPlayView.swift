40e1f2d725fcca9eb501b380d5d0c2da88c509f8
fer0n <rnichi@me.com>
2025-06-30 20:11:23 +0200

    AirPlayButton performance improvements

--- Unwatched/Unwatched/View/Player/AirPlayView.swift
+++ Unwatched/Unwatched/View/Player/AirPlayView.swift
@@ -9,15 +9,32 @@ import AVKit
 import UnwatchedShared
 import OSLog
 
-struct AirPlayButton: View {
+struct AirPlayViewModifier: ViewModifier {
     @Environment(PlayerManager.self) var player
 
+    func body(content: Content) -> some View {
+        content
+            .playerToggleModifier(isOn: isOn, isSmall: true)
+            .contextMenu {
+                Section("autoAirplayHDHelperShort") {
+                    Button {
+                        player.setAirplayHD(!isOn)
+                    } label: {
+                        Text(isOn ? "airplayHDOn" : "airplayHDOff")
+                    }
+                }
+            }
+    }
+
+    var isOn: Bool {
+        player.airplayHD
+    }
+}
+
+struct AirPlayButton: View {
     var body: some View {
         AirPlayView()
-            .playerToggleModifier(isOn: player.airplayHD, isSmall: true)
-            .contextMenu {
-                AirPlayHDButton()
-            }
+            .modifier(AirPlayViewModifier())
             .help("airPlay")
             .accessibilityElement()
             .accessibilityAddTraits(.isButton)
@@ -26,12 +43,18 @@ struct AirPlayButton: View {
 }
 
 struct AirPlayView: View {
-    @State private var routePickerView = AVRoutePickerView()
+    @State private var routePickerView: AVRoutePickerView?
     @State var hapticToggle = false
 
     var body: some View {
         Button {
-            guard let button = routePickerView.subviews.first(where: { $0 is UIButton }) else {
+            if routePickerView == nil {
+                let picker = AVRoutePickerView()
+                picker.isHidden = true
+                picker.prioritizesVideoDevices = true
+                routePickerView = picker
+            }
+            guard let button = routePickerView?.subviews.first(where: { $0 is UIButton }) else {
                 Log.info("AirPlay button not found")
                 return
             }
@@ -41,22 +64,7 @@ struct AirPlayView: View {
             Image(systemName: "airplay.video")
                 .fontWeight(.black)
         }
-        .background {
-            UIKitRoutePickerView(routePickerView: routePickerView)
-        }
         .sensoryFeedback(Const.sensoryFeedback, trigger: hapticToggle)
     }
-}
-
-struct UIKitRoutePickerView: UIViewRepresentable {
-    let routePickerView: AVRoutePickerView
-
-    func makeUIView(context: Context) -> AVRoutePickerView {
-        routePickerView.isHidden = true
-        routePickerView.prioritizesVideoDevices = true
-        return routePickerView
-    }
-
-    func updateUIView(_ uiView: AVRoutePickerView, context: Context) {}
 }
 #endif
