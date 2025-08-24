6a367c73dfb2bb9194b790b931c21332d93dd9a1
fer0n <rnichi@me.com>
2025-08-24 08:32:11 +0200

    larger mini player; removed thumbnail quality workaround

--- Unwatched/Unwatched/View/Player/PlayerView.swift
+++ Unwatched/Unwatched/View/Player/PlayerView.swift
@@ -157,7 +157,6 @@ struct PlayerView: View {
             .frame(maxWidth: !landscapeFullscreen && !hideMiniPlayer ? .infinity : nil)
             .frame(width: !hideMiniPlayer ? 107 : nil,
                    height: !hideMiniPlayer ? 60 : nil)
-            .thumbnailQualityWorkaround(enable: !hideMiniPlayer)
             .padding(.leading, !hideMiniPlayer ? 5 : 0)
 
             if !hideMiniPlayer {
@@ -195,7 +194,6 @@ struct PlayerView: View {
             HStack {
                 if !hideMiniPlayer {
                     thumbnailPlaceholder
-                        .thumbnailQualityWorkaround(enable: !hideMiniPlayer)
                         .padding(.leading, 5)
                     miniPlayerContent
                 }
@@ -349,14 +347,4 @@ struct PlayerView: View {
         .environment(RefreshManager())
         .environment(ImageCacheManager())
         .tint(Color.neutralAccentColor)
-}
-
-extension View {
-    func thumbnailQualityWorkaround(enable: Bool) -> some View {
-        self
-            .scaleEffect(enable ? 0.83 : 1)
-            .padding(.horizontal, enable ? -(107 * 0.17) / 2 : 0)
-        // ^ workaround: if website is loaded while being mini, the cover image resolution is too low
-        // 107 width with 16/9 is the minimum size to still get the higher resolution
-    }
 }
