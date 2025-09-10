b48d07edfd4d03db60fbfbe15701fbd98bab711e
fer0n <rnichi@me.com>
2025-09-10 17:59:52 +0200

    option to "add video" via shortcut to inbox

--- Unwatched/Unwatched/App Intents/AddYoutubeURL.swift
+++ Unwatched/Unwatched/App Intents/AddYoutubeURL.swift
@@ -14,12 +14,15 @@ struct AddYoutubeURL: AppIntent {
     @Parameter(title: "youtubeVideoUrl")
     var youtubeUrl: URL
 
+    @Parameter(title: "destination", default: .queue)
+    var destination: VideoDestination
+
     @MainActor
     func perform() async throws -> some IntentResult {
         Signal.log("Shortcut.AddYoutubeURL")
         let task = VideoService.addForeignUrls(
             [youtubeUrl],
-            in: .queue,
+            in: destination.toCollection(),
             markAsNew: true
         )
         try await task.value
@@ -28,6 +31,31 @@ struct AddYoutubeURL: AppIntent {
     }
 
     static var parameterSummary: some ParameterSummary {
-        Summary("addYoutubeUrl \(\.$youtubeUrl)")
+        Summary("addYoutubeUrl \(\.$youtubeUrl) to \(\.$destination)")
+    }
+}
+
+enum VideoDestination: String, AppEnum {
+    case queue
+    case inbox
+
+    static var typeDisplayRepresentation: TypeDisplayRepresentation {
+        return TypeDisplayRepresentation(name: "destination")
+    }
+
+    static var caseDisplayRepresentations: [VideoDestination: DisplayRepresentation] {
+        [
+            .queue: DisplayRepresentation(title: "queue"),
+            .inbox: DisplayRepresentation(title: "inbox")
+        ]
+    }
+
+    func toCollection() -> VideoPlacementArea {
+        switch self {
+        case .queue:
+            return .queue
+        case .inbox:
+            return .inbox
+        }
     }
 }
