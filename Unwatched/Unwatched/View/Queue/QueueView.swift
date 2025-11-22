a7808e5ba98f04c307a2a75003c0e28095e982e5
fer0n <rnichi@me.com>
2025-11-22 10:20:56 +0100

    queue move animation change

--- Unwatched/Unwatched/View/Queue/QueueView.swift
+++ Unwatched/Unwatched/View/Queue/QueueView.swift
@@ -53,6 +53,7 @@ struct QueueView: View {
                                         isNew: video.isNew,
                                         showAllStatus: false,
                                         clearRole: .destructive,
+                                        queueRole: Const.iOS26 ? .destructive : nil,
                                         clearAboveBelowList: .queue,
                                         showContextMenu: enableQueueContextMenu,
                                         showDelete: false,
