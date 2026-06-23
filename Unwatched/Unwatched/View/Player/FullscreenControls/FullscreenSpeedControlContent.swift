088b64522dbcffce368fc6b02c5f4e5b4c81bdea
fer0n <rnichi@me.com>
2026-06-23 20:45:03 +0200

    fix: mini speed selection stuck on appear

--- Unwatched/Unwatched/View/Player/FullscreenControls/FullscreenSpeedControlContent.swift
+++ Unwatched/Unwatched/View/Player/FullscreenControls/FullscreenSpeedControlContent.swift
@@ -29,7 +29,7 @@ struct FullscreenSpeedControlContent: View {
 
     var body: some View {
         ScrollView {
-            LazyVStack {
+            VStack(spacing: 10) {
                 ForEach(
                     ViewModel.formattedSpeedsEnumerated,
                     id: \.offset
